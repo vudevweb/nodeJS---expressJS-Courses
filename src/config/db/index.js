a72d0dbec8fa860d175392949ccdef27fdc98b8d
Vu Do <vudo.contact.vn@gmail.com>
2024-04-14 15:12:35 +0700

    123123

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education_dev' , {
+        await mongoose.connect('mongodb+srv://vudevweb:vudo354z@cluster0.2o1wq4u.mongodb.net/f8_nodejs', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
