b59cf5cb9ec6a0f8a5d26817d9610904eb1905cf
Vu Do <vudo.contact.vn@gmail.com>
2024-04-15 15:22:32 +0700

    12312

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ var methodOverride = require('method-override')
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
-const db = require('./config/db')
+const route = require('./src/routes')
+const db = require('./src/config/db')
 
 // connect to db
 db.connect()
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'src/public')))
 
 app.use(express.urlencoded({
     extended: true,
 }))
 app.use(express.json())
 app.use(methodOverride('_method'))
+
 // http logger
 app.use(morgan('combined'));
 
@@ -26,15 +27,15 @@ app.use(morgan('combined'));
 app.engine('hbs', exphbs.create({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'resources/views/layouts'),
-    partialsDir: path.join(__dirname, 'resources/views/partials'),
-    pagesDir: path.join(__dirname, 'resources/views/pages'),
+    layoutsDir: path.join(__dirname, 'src/resources/views/layouts'),
+    partialsDir: path.join(__dirname, 'src/resources/views/partials'),
+    pagesDir: path.join(__dirname, 'src/resources/views/pages'),
     helpers: {
         sum: (a , b) => a + b,
     }
 }).engine);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views/pages'));
+app.set('views', path.join(__dirname, 'src/resources/views/pages'));
 // end template engine
 
 // route init 
